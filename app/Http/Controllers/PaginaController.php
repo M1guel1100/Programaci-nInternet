71942650dfe04d71400d773e2008cb7c976facb1
M1guel1100 <m1guel110_lopez@hotmail.com>
2022-10-03 21:58:54 -0500

    insersión base de datos

--- app/Http/Controllers/PaginaController.php
+++ app/Http/Controllers/PaginaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Contacto;
 
 class PaginaController extends Controller
 {
@@ -29,13 +30,26 @@ class PaginaController extends Controller
             'comentario' => 'required|min:5',
         ]);
 
-        DB::table('contactos')->insert([
+       /* DB::table('contactos')->insert([
             'nombre' => $request->nombre,
             'correo'=> $request->correo,
             'comentario'=>$request->comentario,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+        */
+        //DB::table('contactos')->insert($request->except('_token'));
+
+        /*$contacto = new Contacto();
+        $contacto->nombre = $request->nombre;
+        $contacto->correo = $request->correo;
+        $contacto->comentario = $request->comentario;
+        $contacto->save();*/
+
+        Contacto::create($request->all());
+
         return redirect('/contacto');
+    
+
     }
 }
